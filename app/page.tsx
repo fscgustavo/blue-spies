70b45062d08d30a517d3c26cb93f77f401d3da18
Gustavo Fonseca <gus.fonnseca@gmail.com>
2024-10-07 22:11:46 -0300

    prettier and eslint issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { Suspense } from 'react';
+
 import { FavoriteFeed } from '@/components/favorite-feed';
 import { Menu } from '@/components/menu';
 
@@ -7,8 +9,10 @@ export default function Home() {
   return (
     <main className="h-screen">
       <div className="mx-auto grid min-h-dvh max-w-7xl auto-rows-min gap-6 lg:grid-cols-[minmax(auto,_17.18rem)_minmax(auto,_37.5rem)_auto]">
-        <Menu />
-        <FavoriteFeed />
+        <Suspense>
+          <Menu />
+          <FavoriteFeed />
+        </Suspense>
       </div>
     </main>
   );
