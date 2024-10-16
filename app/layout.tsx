2ec2893f6a51574716c6e3cbff0617a4e7da59ab
Gustavo Fonseca <gus.fonnseca@gmail.com>
2024-10-16 10:13:21 -0300

    chore: add canonical

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,18 @@
 import './globals.css';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  alternates: {
+    canonical: '/',
+    languages: {
+      pt: '/pt',
+      en: '/en',
+      es: '/es',
+    },
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
